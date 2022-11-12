abcb135fe3b1f64a85fbaa5ff59ab8f404ae8684
Nikita <mail@gmail.com>
2022-11-13 00:42:05 +0300

    Finished Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int num1 = num % 2;
+
+if (num1 == 0)
+{
+    Console.WriteLine($"{num} -> да");
+}
+else
+{
+    Console.WriteLine($"{num} -> нет");
+}
