732098f813b08b2045ec153b02d2289b32c0e488
Nikita <mail@gmail.com>
2022-11-13 00:30:31 +0300

    Finished Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (num1 > num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+
+Console.WriteLine($"{num1}, {num2}, {num3} -> {max}");
