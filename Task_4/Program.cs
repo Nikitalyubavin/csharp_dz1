cfe30b02ea820cb95b0b67dae8d343f01030305a
Nikita <mail@gmail.com>
2022-11-13 18:45:33 +0300

    Finished Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите целое число: ");
+int n = int.Parse(Console.ReadLine());
+
+int a = 1;
+
+int s = 0;
+
+Console.Write($"{n} -> ");
+
+while (a < n - 1)
+{
+    s = a % 2;
+    if (s == 0)
+    {        
+        Console.Write($"{a}, ");
+        a = a + 1;
+    }
+    else
+    {
+        a = a + 1;
+    }
+}
+
+while (a <= n)
+{
+    s = a % 2;
+    if (s == 0)
+    {        
+        Console.Write($"{a}");
+        a = a + 1;
+    }
+    else
+    {
+        a = a + 1;
+    }
+}
